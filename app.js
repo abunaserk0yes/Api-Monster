f14147ab8cb7365646267e02a91290670d1549bc
Md Abu Naser Kayes <abunaserkayes3305@yahoo.com>
2021-02-07 22:23:08 +0600

    massage

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 document.getElementById('searchBtn').addEventListener('click', function () {
-    const searchItem = document.getElementById('searchItem').value
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchItem}`)
-        .then(response => response.json())
+    const itemsName = document.getElementById('searchItem').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${ itemsName }`)
+        .then(res => res.json())
         .then(data => {
-            console.log(data)
-            const foodItem = document.getElementById('foodItems');
+            const foodsItem = document.getElementById("foodsItem");
             data.meals.forEach(element => {
-                const foodDiv = document.createElement('div');
+                const foodDiv = document.createElement('div')
                 foodDiv.innerHTML = `
-                    <img onclick="detailInfoMeals('data')" src="${element.strMealThumb}">
-                    <h1>${element.strMeal}</h1>
-                `
-                foodDiv.className="foodBioData";
-                foodItem.appendChild(foodDiv);
+            <img src="${ element.strMealThumb }" onClick="checkItemsInfo(${ element.idMeal })">    
+            <h1 onClick="checkItemsInfo(${ element.idMeal })" >${ element.strMeal }</h1>
+            `;
+                foodDiv.className = "card";
+                foodsItem.appendChild(foodDiv);
             });
         })
 })
 
-const detailInfoMeals=foodId=>{
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)
-    .then(response=>response.json())
-    .then(data=>{
-        const foodInfo=document.createElement('div');
-        foodInfo.innerHTML=`
-            <img src='${data.meals[0].strMealThumb}'>
-            <h3>${data.meals[0].strMeal}</h3>
-            <h4>${data.meals[0].strCategory}</h4>
+let checkItemsInfo = foodId => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ foodId }`)
+        .then(res => res.json())
+        .then(data => {
+            let foodInfo = document.getElementById('foodInfo');
+            document.getElementById('foodInfo').style.display = 'block';
+            let foodDetails = document.createElement('div')
+            foodDetails.innerHTML = `
+            <img src="${ data.meals[0].strMealThumb }">
+ 		    <h1>${ data.meals[0].strMeal }</h1>
             <br>
-            <h5></h5>
-            <h5></h5>
-            <h5></h5>
-            <h5></h5>
-            <h5></h5>
-        `
-    })
+            <h3><i class="fas fa-check-square"></i> ${ data.meals[0].strMeasure1 }</h3>
+            <h3><i class="fas fa-check-square"></i> ${ data.meals[0].strMeasure2 }</h3>
+            <h3><i class="fas fa-check-square"></i> ${ data.meals[0].strMeasure3 }</h3>
+            `;
+            foodDetails.className = "food-details";
+            foodInfo.appendChild(foodDetails);
+        })
 }
