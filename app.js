c0aa7d90b03ebcbb662584d88b22d3a69e6c1b36
Md Abu Naser Kayes <abunaserkayes3305@yahoo.com>
2021-02-09 22:50:55 +0600

    sorry

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
-document.getElementById('searchBtn').addEventListener('click', function () {
-    const itemsName = document.getElementById('searchItem').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${ itemsName }`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            const foodDetail = document.getElementById("foodDetail");
-            data.meals.forEach(element => {
-                const foodDiv = document.createElement('div')
-                foodDiv.innerHTML = `
-            <img src="${ element.strMealThumb }" onClick="checkItemsInfo(${ element.idMeal })">    
-            <h1 onClick="checkItemsInfo(${ element.idMeal })" >${ element.strMeal }</h1>
-            `;
-                foodDiv.className = "foodDivClass";
-                foodDetail.appendChild(foodDiv);
-            });
-        })
-})
+const searchMealBtn=()=>{
+    const SearchMeal=document.getElementById('searchMeal').value;
+    document.getElementById('searchMeal').value=''
+    const url=`https://www.themealdb.com/api/json/v1/1/search.php?f=${SearchMeal}`
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>displayMeals(data.meals))
+}
+const displayMeals=meals=>{
+    const mealsContainer=document.getElementById('meals-container')
+    meals.forEach(meal =>{
+        const mealDiv=document.createElement('div');
+        mealDiv.className='mealDivAll'
+        mealDiv.innerHTML=`
+        <h2 onclick="displayMealsDetails('${meal.idMeal}')">${meal.strMeal}</h2>
+        <img onclick="displayMealsDetails('${meal.idMeal}')" src="${meal.strMealThumb}">
+        `
+        mealsContainer.appendChild(mealDiv);
+    });
 
-let checkItemsInfo = foodId => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ foodId }`)
-        .then(res => res.json())
-        .then(data => {
-            let foodInfo = document.getElementById('foodInfo');
-            document.getElementById('foodInfo').style.display = 'block';
-            let foodQfInfo = document.createElement('div')
-            foodQfInfo.innerHTML = `
-            <img src="${ data.meals[0].strMealThumb }">
- 		    <h1>${ data.meals[0].strMeal }</h1>
-            <br>
-            <h5><i class="fas fa-check-square"></i> ${ data.meals[0].strMeasure1 }</h5>
-            <h5><i class="fas fa-check-square"></i> ${ data.meals[0].strMeasure2 }</h5>
-            <h5><i class="fas fa-check-square"></i> ${ data.meals[0].strMeasure3 }</h5>
-
-            <p>${data.meals[0].strInstructions}</p>
-            `;
-            foodQfInfo.className = "food-details";
-            foodInfo.appendChild(foodQfInfo);
-        })
+}
+const displayMealsDetails=idMeal=>{
+    const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>{
+        // console.log(data)
+        const mealsDetails=document.getElementById('meals-details');
+        const mealsDetailsDiv=document.createElement('div');
+        mealsDetailsDiv.className='mealsDetailsInfo';
+        mealsDetailsDiv.innerHTML=`
+        <h3>${data.meals[0].strMeal}</h3>
+        <img src="${data.meals[0].strMealThumb}">
+        <h5><i class="fas fa-check-square"></i>${data.meals[0].strIngredient1}</h5>
+        <h5><i class="fas fa-check-square"></i>${data.meals[0].strIngredient2}</h5>
+        <h5><i class="fas fa-check-square"></i>${data.meals[0].strIngredient3}</h5>
+        <h5><i class="fas fa-check-square"></i>${data.meals[0].strIngredient4}</h5>
+        <h5><i class="fas fa-check-square"></i>${data.meals[0].strIngredient5}</h5>
+        `
+        mealsDetails.appendChild(mealsDetailsDiv)
+    })
 }
